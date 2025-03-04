5d025cfbdcfb864b61b80d91d3473c18b19666d3
dimasma0305 <dimasmaulana0305@gmail.com>
2025-03-04 09:24:31 +0000

    change images name

--- images/build.sh
+++ images/build.sh
@@ -3,6 +3,6 @@
 set -e
 
 (cd challenge-base && docker build  . -t gcr.io/paradigmxyz/ctf/base:latest)
-(cd challenge && docker build . -f Dockerfile.cairo -t gcr.io/paradigmxyz/ctf/cairo-base:latest)
-(cd challenge && docker build . -f Dockerfile.eth -t gcr.io/paradigmxyz/ctf/eth-base:latest)
-(cd challenge && docker build . -f Dockerfile.solana -t gcr.io/paradigmxyz/ctf/solana-base:latest)
+(cd challenge && docker build . -f Dockerfile.cairo -t dimasmaualana/cairo:latest)
+(cd challenge && docker build . -f Dockerfile.eth -t dimasmaualana/eth:latest)
+(cd challenge && docker build . -f Dockerfile.solana -t dimasmaualana/solana:latest)
